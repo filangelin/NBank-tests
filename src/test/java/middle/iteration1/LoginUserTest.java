70a9d6050c805a4ce03bced3743ea27a44399365
Angelina Matveeva <fil.angeliin@gmail.com>
2025-11-16 04:08:35 +0300

    add iteration1 tests middle level

---
+++ src/test/java/middle/iteration1/LoginUserTest.java
@@ -0,0 +1,46 @@
+package middle.iteration1;
+
+import middle.generators.RandomData;
+import middle.models.CreateUserRequest;
+import middle.models.LoginUserRequest;
+import middle.models.UserRole;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import middle.requests.AdminCreateUserRequester;
+import middle.requests.LoginUserRequester;
+import middle.specs.RequestSpecs;
+import middle.specs.ResponseSpecs;
+
+public class LoginUserTest extends BaseTest {
+
+    @Test
+    public void adminCanGenerateAuthTokenTest() {
+        LoginUserRequest userRequest = LoginUserRequest.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        new LoginUserRequester(RequestSpecs.unauthSpec(),
+                ResponseSpecs.requestReturnsOK())
+                .post(userRequest);
+    }
+
+    @Test
+    public void userCanGenerateAuthTokenTest() {
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+                .username(RandomData.getUsername())
+                .password(RandomData.getPassword())
+                .role(UserRole.USER.toString())
+                .build();
+
+        new AdminCreateUserRequester(
+                RequestSpecs.adminSpec(),
+                ResponseSpecs.entityWasCreated())
+                .post(userRequest);
+
+        new LoginUserRequester(RequestSpecs.unauthSpec(),
+                ResponseSpecs.requestReturnsOK())
+                .post(LoginUserRequest.builder().username(userRequest.getUsername()).password(userRequest.getPassword()).build())
+                .header("Authorization", Matchers.notNullValue());
+    }
+}
